a1e4f5e62a0864b077b0683fa4ebbf1ea6c67b62
Javi <javi_castillo18@hotmail.com>
2016-02-24 18:37:05 +0100

    Detail screen and launch

--- Marvel/Marvel/Controllers/CharacterDetailViewController.swift
+++ Marvel/Marvel/Controllers/CharacterDetailViewController.swift
@@ -24,6 +24,7 @@ class CharacterDetailViewController: UIViewController {
         else {
             descriptionCharacter.text = self.character.descriptionCharacter
         }
+		imageCharacter.contentMode = UIViewContentMode.ScaleAspectFit;
         imageCharacter.image = character.imageThumbnail
     }
     
