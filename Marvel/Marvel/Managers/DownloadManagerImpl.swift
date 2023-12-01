796699bde7b9f245197362daa5bf1ade7b95f185
Javi <javi_castillo18@hotmail.com>
2023-12-01 09:18:54 +0100

    Return in the guard

--- Marvel/Marvel/Managers/DownloadManagerImpl.swift
+++ Marvel/Marvel/Managers/DownloadManagerImpl.swift
@@ -107,7 +107,6 @@ class DownloadManagerImpl: DownloadManager {
         let task = URLSession.shared.dataTask(with: currentUrl, completionHandler: { [self] data, response, error in
             if error == nil {
                 guard let httpUrlResponse = response as? HTTPURLResponse else {
-                    return
                 }
 				let statusCode = httpUrlResponse.statusCode
 				switch statusCode {
