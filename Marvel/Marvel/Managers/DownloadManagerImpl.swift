996d7e1ac9bc180ac6232d84b9bf9a050587a406
Javi <javi_castillo18@hotmail.com>
2023-12-03 13:24:33 +0100

    Add guard

--- Marvel/Marvel/Managers/DownloadManagerImpl.swift
+++ Marvel/Marvel/Managers/DownloadManagerImpl.swift
@@ -107,7 +107,6 @@ class DownloadManagerImpl: DownloadManager {
         let task = URLSession.shared.dataTask(with: currentUrl, completionHandler: { [self] data, response, error in
             if error == nil {
 				do {
-                    guard let data = data,
                     }
                     delegate?.infoDownloaded(info: info)
                 } catch let parseError {
