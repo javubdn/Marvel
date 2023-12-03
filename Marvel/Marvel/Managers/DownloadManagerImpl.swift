c10992bf01f5210d5dffd03556651ce82321dda2
Javi <javi_castillo18@hotmail.com>
2023-12-03 13:24:12 +0100

    Add json condition for guard

--- Marvel/Marvel/Managers/DownloadManagerImpl.swift
+++ Marvel/Marvel/Managers/DownloadManagerImpl.swift
@@ -108,7 +108,6 @@ class DownloadManagerImpl: DownloadManager {
             if error == nil {
 				do {
                     guard let data = data,
-                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary,
                         let info = json["data"] as? [String: Any] else {
                     }
                     delegate?.infoDownloaded(info: info)
