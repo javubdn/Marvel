c7a2a6758f4065829ab8606bfbce1a746767c127
Javi <javi_castillo18@hotmail.com>
2023-12-06 17:50:19 +0100

    Validate in guard json

--- Marvel/Marvel/Managers/DownloadManagerImpl.swift
+++ Marvel/Marvel/Managers/DownloadManagerImpl.swift
@@ -111,6 +111,7 @@ class DownloadManagerImpl: DownloadManager {
                 //TODO: We should give a message to the user indicating that the maximum of requests has been achieved
                 return
             }
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary,
                 }
             }
         })
