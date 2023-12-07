717be7b361eb6124782c9836359b550d1bae0c21
Javi <javi_castillo18@hotmail.com>
2023-12-07 08:57:52 +0100

    Print error in guard

--- Marvel/Marvel/Managers/DownloadManagerImpl.swift
+++ Marvel/Marvel/Managers/DownloadManagerImpl.swift
@@ -114,6 +114,7 @@ class DownloadManagerImpl: DownloadManager {
                 guard let data = data,
                     let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary,
                     let info = json["data"] as? [String: Any] else {
+                        print("Error could not parse JSON")
                 }
             }
         })
